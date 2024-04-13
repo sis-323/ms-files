94ac60d93f1f4d6ceebc2e4e07421795410ca796
Sergio Guachalla <sergioguachalla0@gmail.com>
2024-04-13 13:58:25 -0400

    first commit

---
+++ src/test/kotlin/com/files/msfiles/MsFilesApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.files.msfiles
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MsFilesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
