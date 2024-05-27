327fe6134fd53a3aac62bd96d3f5b8ddb5d5c17a
iguii <igui10082002@gmail.com>
2024-05-26 23:39:36 -0400

    Remove CORS config

--- src/main/kotlin/com/files/msfiles/api/FileApi.kt
+++ src/main/kotlin/com/files/msfiles/api/FileApi.kt
@@ -9,7 +9,6 @@ import org.springframework.web.multipart.MultipartFile
 
 @RestController
 @RequestMapping("/api/v1/files")
-@CrossOrigin(origins = ["*"])
 class FileApi constructor(
         private val fileBl: FileBl,
     private val deliverableFileBl: DeliverableFileBl
